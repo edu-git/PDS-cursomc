07f18feda22f71ff6bf921c58ba8b099ce56b1d3
Eduardo Rodrigues <eduardo.jam@gmail.com>
2017-09-21 21:50:18 -0300

    Endpoint /cliente/{id} dispoinivel

---
+++ src/main/java/com/pds/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.pds.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pds.cursomc.domain.Cliente;
+import com.pds.cursomc.repositories.ClienteRepository;
+import com.pds.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+id
+					+ ", tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
