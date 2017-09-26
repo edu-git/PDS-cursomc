173f334e30152d0ec31e147d81914fc009ef9272
Eduardo Rodrigues <eduardo.jam@gmail.com>
2017-09-26 16:41:08 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/pds/cursomc/CursomcApplication.java
+++ src/main/java/com/pds/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.pds.cursomc.domain.Cidade;
 import com.pds.cursomc.domain.Cliente;
 import com.pds.cursomc.domain.Endereco;
 import com.pds.cursomc.domain.Estado;
+import com.pds.cursomc.domain.ItemPedido;
 import com.pds.cursomc.domain.Pagamento;
 import com.pds.cursomc.domain.PagamentoComBoleto;
 import com.pds.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.pds.cursomc.repositories.CidadeRepository;
 import com.pds.cursomc.repositories.ClienteRepository;
 import com.pds.cursomc.repositories.EnderecoRepository;
 import com.pds.cursomc.repositories.EstadoRepository;
+import com.pds.cursomc.repositories.ItemPedidoRepository;
 import com.pds.cursomc.repositories.PagamentoRepository;
 import com.pds.cursomc.repositories.PedidoRepository;
 import com.pds.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -111,6 +115,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.save(Arrays.asList(ped1,ped2));
 		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.save(Arrays.asList(ip1,ip2,ip3));
+		
 		
 	}
 		
--- src/main/java/com/pds/cursomc/domain/Produto.java
+++ src/main/java/com/pds/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.pds.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,11 +13,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto  implements Serializable{
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -29,8 +32,11 @@ public class Produto  implements Serializable{
 	@JoinTable(name="PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name="produto_id"),
 		inverseJoinColumns = @JoinColumn(name="categoria_id")
-			)
+	)
 	private List<Categoria> categorias = new ArrayList<>();
+	
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Produto() {
 	}
@@ -41,7 +47,15 @@ public class Produto  implements Serializable{
 		this.nome = nome;
 		this.preco = preco;
 	}
-
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -73,6 +87,14 @@ public class Produto  implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -97,8 +119,5 @@ public class Produto  implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
-	
-	
+	}	
 }
