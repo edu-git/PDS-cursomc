d1f99ac22a8ba0e4a3b6adcb8d8bc02ab3a54cdf
Eduardo Rodrigues <eduardo.jam@gmail.com>
2017-10-17 22:00:13 -0200

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/pds/cursomc/services/ClienteService.java
+++ src/main/java/com/pds/cursomc/services/ClienteService.java
@@ -61,7 +61,7 @@ public class ClienteService {
 			repo.delete(id);
 		}
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir pois há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir pois há pedidos relacionados");
 		}
 	}
 
