4fd9ede072f629888bf463e81c6b5eecf0cc1c0a
Eduardo Rodrigues <eduardo.jam@gmail.com>
2017-11-21 21:49:20 -0200

    Adicionando senha a Cliente

--- src/main/java/com/pds/cursomc/services/DBService.java
+++ src/main/java/com/pds/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pds.cursomc.domain.Categoria;
@@ -34,6 +35,8 @@ import com.pds.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService {
 	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -111,7 +114,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1,est2));
 		cidadeRepository.save(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","eduardo.jam@gmail.com","36378912377", TipoCliente.PESSOASFISICA); 
+		Cliente cli1 = new Cliente(null,"Maria Silva","eduardo.jam@gmail.com","36378912377", TipoCliente.PESSOASFISICA, pe.encode("123")); 
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","38220834",cli1,c1);
